7ca3d6f4f1a04e873162c010de5c032802ef1891
Olaf Minkowicz <olaf.a.minkowicz@gmail.com>
2021-01-29 23:13:06 -0800

    docs: changing title, desc and owner in gastby-config

--- arcanejs/gatsby-config.js
+++ arcanejs/gatsby-config.js
@@ -2,10 +2,10 @@ const path = require('path')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter MDX Basic',
+    title: 'ArcaneJS',
     description:
-      'Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.',
-    author: '@chrisbiscardi',
+      'Demystifying the mystery that is JavaScript!',
+    author: 'Olaf @RookTKO Minkowicz',
   },
   plugins: [
     `gatsby-plugin-sass`,
