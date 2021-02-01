7f8769a2dcbf1f188ac000dcda61eb433a985b32
Olaf Minkowicz <olaf.a.minkowicz@gmail.com>
2021-01-31 23:23:34 -0800

    feat: adding logo scafolding

--- arcanejs/src/components/header.js
+++ arcanejs/src/components/header.js
@@ -1,34 +1,24 @@
 import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
+import Emoji from './widgets/emoji'
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
+  <>
+  <Emoji />
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
-            color: 'white',
+            color: 'black',
             textDecoration: 'none',
           }}
         >
           {siteTitle}
         </Link>
       </h1>
-    </div>
-  </div>
+  <Emoji />
+  </>
 )
 
 Header.propTypes = {
---
+++ arcanejs/src/components/widgets/emoji.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './emoji.scss';
+
+const Emoji = () => (
+<div style={{postion: "absolute"}}>
+  <div className="center">
+    <span classNameName="crystalball"></span>
+    <span className="emoji-hat-border">
+      <span className="emoji-hat">
+        <span className="emoji-hat-star-1"></span>
+        <span className="emoji-hat-star-2"></span>
+        <span className="emoji-hat-star-3"></span>
+        <span className="emoji-hat-star-4"></span>
+        <span className="emoji-hat-star-5"></span>
+      </span>
+    </span>
+    <span className="emoji-body emoji-face-anime">
+
+      <span className="emoji-sparklingeyes-upper"></span>
+      <span className="emoji-sparklingeyes-lower"></span>
+      <span className="mouth"></span>
+    </span>
+
+    <span className="emoji-hands"></span>
+    <span className="crystalball"></span>
+  </div>
+</div>
+)
+
+
+export default Emoji;
